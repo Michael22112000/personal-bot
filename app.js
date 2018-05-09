b81497f69e8173b567b5f60e17501d1d0657d241
Michael22112000 <mt22112000@gmail.com>
2018-05-09 16:31:51 +0100

    Update app.js

--- app.js
+++ app.js
@@ -37,12 +37,12 @@ client.on("ready", async () => {
 
 
             //if 72 hours have passed, remove from config
-            //if(date < new Date().getTime() - 259200000) {
-                 //delete client.colors[i];
-                 //fs.writeFile("./servers.json", JSON.stringify(client.colors, null, 4), err => {
-                    //if(err) throw err;
-                //});
-                //return;
+            if(date < new Date().getTime() - 259200000) {
+                 delete client.colors[i];
+                 fs.writeFile("./servers.json", JSON.stringify(client.colors, null, 4), err => {
+                    if(err) throw err;
+                });
+                return;
             }
 
             //if server gets deleted or bot gets kicked, remove from config
